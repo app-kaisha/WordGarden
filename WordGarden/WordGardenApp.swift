0a432c220977fdea58cdc6000c2107ce8097fd33
app-kaisha <app.kaihatsusha@gmail.com>
2025-12-30 13:46:04 +0000

    Initial Commit

---
+++ WordGarden/WordGardenApp.swift
@@ -0,0 +1,18 @@
+//
+//  WordGardenApp.swift
+//  WordGarden
+//
+//  Created by app-kaihatsusha on 30/12/2025.
+//  Copyright © 2025 app-kaihatsusha. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct WordGardenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
